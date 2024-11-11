a9db7383bef8baa3c6e5e4d96fd13c6b294675ac
Taha Jamal <taha.jamal@uvm.edu>
2024-11-11 10:05:20 -0500

    commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessesRemaining = 3;
+
+document.getElementById("guessButton").addEventListener("click", guessNumber);
+
+function guessNumber() {
+  if (guessesRemaining === 0) {
+    document.getElementById("message").textContent = "You've run out of guesses. The correct number was " + randomNumber;
+    document.getElementById("guessInput").disabled = true;
+    document.getElementById("guessButton").disabled = true;
+    return;
+  }
+
+  let guess = parseInt(document.getElementById("guessInput").value);
+  guessesRemaining--;
+
+  if (guess === randomNumber) {
+    document.getElementById("message").textContent = "Congratulations! You guessed correctly.";
+    document.getElementById("guessInput").disabled = true;
+    document.getElementById("guessButton").disabled = true;
+  } else if (guess < randomNumber) {
+    document.getElementById("message").textContent = "Too low. Guesses remaining: " + guessesRemaining;
+  } else {
+    document.getElementById("message").textContent = "Too high. Guesses remaining: " + guessesRemaining;
+  }
+}
